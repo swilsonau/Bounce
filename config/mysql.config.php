3c990edd15534bbca4767bb6d8aef2cec409e161
Stewart Wilson <stewartwilson@outlook.com>
2014-07-29 13:54:58 +1000

    pure css layout, first changes

---
+++ config/mysql.config.php
@@ -0,0 +1,9 @@
+<?php
+// Project Bounce
+// ISIT351 Project @ UOW 2014
+
+// Code by Stewart Wilson 4267084
+
+mysql_connect("localhost", "root", "") or die (mysql_error());
+mysql_select_db("bounceapp") or die(mysql_error());
+?>
