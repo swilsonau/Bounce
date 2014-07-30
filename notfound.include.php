720b0fa6e1bba9b13213fc1ec8afa72e983d66da
Stewart Wilson <stewartwilson@outlook.com>
2014-07-30 12:04:22 +1000

    style changes

---
+++ notfound.include.php
@@ -0,0 +1,10 @@
+
+<div class="content-wrapper">
+    <div class="content">
+        <h2 class="content-head is-center">404 Page Not Found</h2>
+        <p style="text-align: center;">Sorry but the page you requested isn't available right now.</p>
+        <p style="text-align: center;"><a href="<?php echo $siteurl; ?>">Go Back to Home</a></p>
+
+    </div>
+
+</div>
