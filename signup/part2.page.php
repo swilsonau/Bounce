1d7f1cffaf4ec192066b4aeb7fe16b0293199cad
Stewart Wilson <stewartwilson@outlook.com>
2014-08-06 14:02:51 +1000

    part2

--- signup/part2.page.php
+++ signup/part2.page.php
@@ -30,12 +30,24 @@ if(!isset($_SESSION['bouncenewid'])) {
   <div class="pure-g">
       <div class="pure-u-1 pure-u-md-1-2">
         <h2>I'm a client</h2>
-        <p>I want to use <?php echo $sitename; ?> to communicate with my Personal Trainer</p>
+        <p>I want to use <?php echo $sitename; ?> to communicate with my Personal Trainer.</p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1645.9274379054707!2d150.8790415436596!3d-34.40503443087485!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b131971e23b2943%3A0x28106b564503fe41!2sUniversity+of+Wollongong!5e0!3m2!1sen!2s!4v1407294237338" width="400" height="300" frameborder="0" style="border:0"></iframe><br />
+        <em>Evenutally, you'll be able to pick a local PT business here. For now, you'll just be assigned to the default.</em><br />
+        <form action="<?php echo $siteurl; ?>signup/part2/process" method="post">
+          <input type="hidden" name="ptid" value="1">
+          <input type="hidden" name="type" value="1">
+          <button type="submit" class="pure-button pure-button-primary">Complete Sign Up</button>
+        </form>
       </div>
 
       <div class="pure-u-1 pure-u-md-1-2">
         <h2>I'm a PT</h2>
         <p>I want to use <?php echo $sitename; ?> to communcate with my own clients</p>
+
+        <form action="<?php echo $siteurl; ?>signup/part2/process" method="post">
+          <input type="hidden" name="type" value="2">
+          <button type="submit" class="pure-button pure-button-primary">Complete Sign Up</button>
+        </form>
       </div>
   </div>
 </div>
