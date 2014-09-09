31bc793928a07475e0568d6643b4f24f029c0d39
Stewart Wilson <stewartwilson@outlook.com>
2014-09-09 16:59:17 +1000

    fitbit stuff

--- crons/fitbitnotifications.php
+++ crons/fitbitnotifications.php
@@ -9,31 +9,46 @@ include('../libs/temboo/temboo.php');
 // Receive notification from fitbit
 $notificationData = file_get_contents($_FILES['updates']['tmp_name']);
 
-mail("sw730@uowmail.edu.au", "fitbit stuff", $notificationData);
-
-die();
-
 $jsondata = json_decode($notificationData);
 
-$token = $jsondata;
+mail("sw730@uowmail.edu.au", "fitbit stuff", $notificationData. '\n'.print_r($jsondata, true));
 
-// Get the list of users currently using fitbit
-$fbf_sql = mysqli_query($sql, "SELECT `u_id`, `AccessToken`, `AccessTokenSecret` FROM `connectedapps` WHERE `app` = 'fitbit' AND `UserID` = '$token'");
+$token = $jsondata['ownerId'];
 
-if(!$fbf_sql) {
+// Get the list
+$fbnot_sql = mysqli_query($sql, "SELECT `UserID`, `status` FROM `fitbit_notif` WHERE `UserID` = '$token' AND `status` = '0'");
+
+if(!$fbnot_sql) {
   echo 'Cannot run';
   sendemail("sw730@uowmail.edu.au", "Fitbit Cron MySQL Error", "Error: ".mysql_error());
 } else {
-  if(mysqli_num_rows($fbf_sql) == 0) {
-    echo 'No users using fitbit';
+  if(mysqli_num_rows($fbnot_sql) == 0) {
+    echo 'Already exists. We know! Thanks.';
   } else {
-    while($fbf = mysqli_fetch_array($fbf_sql)) {
-      $accesstoken = $fbf['AccessToken'];
-      $accesssecret = $fbf['AccessTokenSecret'];
 
-      echo $accesstoken.' '.$accesssecret;
+    // Check that the user exists.
 
+    // Get the list of users currently using fitbit
+    $fbf_sql = mysqli_query($sql, "SELECT `u_id`, `AccessToken`, `AccessTokenSecret` FROM `connectedapps` WHERE `app` = 'fitbit' AND `UserID` = '$token'");
 
+    if(!$fbf_sql) {
+      echo 'Cannot run';
+      sendemail("sw730@uowmail.edu.au", "Fitbit Cron MySQL Error", "Error: ".mysql_error());
+    } else {
+      if(mysqli_num_rows($fbf_sql) == 0) {
+        echo 'No user using fitbit';
+      } else {
+        // put it into the db
+        $date = time();
+
+        $insnotif_sql = mysqli_query($sql, "INSERT INTO `fitbit_notif`(`UserID`, `date`)VALUES('$token', '$date')");
+
+        if(!$insnotif_sql) {
+          echo 'Nope';
+        } else {
+          header('HTTP/1.0 204 No Content');
+        }
+      }
     }
   }
 }
