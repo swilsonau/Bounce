dd3d7726fb77f6b1b25690c5c18ca9495c1954d0
Stewart Wilson <stewartwilson@outlook.com>
2014-10-06 23:43:45 +1100

    changes

--- account/myprograms.page.php
+++ account/myprograms.page.php
@@ -30,7 +30,24 @@ $userdetails = fetchuserdetail($_SESSION['bounceuser']);
         </div>
 
         <div class="pure-u-1 pure-u-md-3-4">
-          
+          <?php
+          // Get users current
+          $userid = $userdetails['id'];
+
+          $usprgsq = mysqli_query($sql, "SELECT * FROM `programs` LEFT JOIN `program_assign` ON `program_assign`.`progid` = `programs`.`id` WHERE `program_assign`.`userid` = '$userid'");
+
+          if(!$usprgsq) {
+            echo "Error";
+          } else {
+            if(mysqli_num_rows($usprgsq) == 0) {
+              echo 'You are not assigned to any programs!';
+            } else {
+              while($usrprog = mysqli_fetch_array($usprgsq)) {
+                echo $usrprog['programname'];
+              }
+            }
+          }
+          ?>
         </div>
   </div>
 </div>
