cf323cbe2da54bc428667d44394cbd6f0195e35e
Stewart Wilson <stewartwilson@outlook.com>
2014-10-07 00:41:06 +1100

    program progress, footer change

--- account/myprograms.page.php
+++ account/myprograms.page.php
@@ -30,6 +30,22 @@ $userdetails = fetchuserdetail($_SESSION['bounceuser']);
         </div>
 
         <div class="pure-u-1 pure-u-md-3-4">
+          <aside>
+            <p><i class="fa fa-info-circle"></i> This is a list of the current programs you're assigned to. You can view information or withdraw from a program here.</p>
+          </aside>
+
+          <table class="pure-table" width="100%">
+            <thead>
+              <tr>
+                <td width="30%">Program Name</td>
+                <td width="20%">Program Date</td>
+                <td width="20%">Program Duration</td>
+                <td width="10%">Status</td>
+                <td width="20%"></td>
+              </tr>
+            </thead>
+
+            <tbody>
           <?php
           // Get users current
           $userid = $userdetails['id'];
@@ -37,17 +53,33 @@ $userdetails = fetchuserdetail($_SESSION['bounceuser']);
           $usprgsq = mysqli_query($sql, "SELECT * FROM `programs` LEFT JOIN `program_assign` ON `program_assign`.`progid` = `programs`.`id` WHERE `program_assign`.`userid` = '$userid'");
 
           if(!$usprgsq) {
-            echo "Error";
+            echo "<tr>
+              <td colspan=\"5\">
+                Sorry, there was an issue processing that request. Please try again.
+              </tr>
+            </tr>";
           } else {
             if(mysqli_num_rows($usprgsq) == 0) {
-              echo 'You are not assigned to any programs!';
+              echo "<tr>
+                <td colspan=\"5\">
+                  Whoops! You aren't assigned to any programs. <a href=\"$siteurl\">Join One Now</a>.
+                </tr>
+              </tr>";
             } else {
               while($usrprog = mysqli_fetch_array($usprgsq)) {
-                echo $usrprog['programname'];
+                echo '<tr>
+                <td>'.$usrprog['programname'].'</td>
+                <td></td>
+                <td>'.dateDiff($usrprog['timestart'], $usrprog['timeend']).'</td>
+                <td><i class="fa fa-check"></i> Enrolled</td>
+                <td style="text-align: right;"><button class="pure-button"><i class="fa fa-search"></i> View Details</button> <button class="pure-button pure-button-error"><i class="fa fa-external-link-square"></i> Withdraw</button></td>
+                </tr>';
               }
             }
           }
           ?>
+        </tbody>
+      </table>
         </div>
   </div>
 </div>
